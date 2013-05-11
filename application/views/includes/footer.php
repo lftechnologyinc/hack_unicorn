1ad8c62629440dc294abb18f2ecfe94080792a35
subash <connect.subash@gmail.com>
2013-05-11 15:20:47 +0545

    css modified

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -4,11 +4,5 @@
       </div>
 
     </div> <!-- /container -->
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-   
-
   </body>
 </html>
